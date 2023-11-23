9c04aaf932e5bc4199f91d2b87563b556627cfe3
pravunathsingh <pravunathsingh@gmail.com>
2023-11-23 11:55:46 +0530

    :sparkles: feat(root): initial project setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,41 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import '@mantine/core/styles.css';
+import '@mantine/notifications/styles.css';
+import {
+  MantineProvider,
+  createTheme,
+  MantineColorsTuple,
+} from '@mantine/core';
+import type { AppProps } from 'next/app';
+import { ModalsProvider } from '@mantine/modals';
+import { Notifications } from '@mantine/notifications';
+
+const myColor: MantineColorsTuple = [
+  '#ffebff',
+  '#f5d5fc',
+  '#e6a9f3',
+  '#d779eb',
+  '#cb51e4',
+  '#c437e0',
+  '#c029df',
+  '#a91cc6',
+  '#9715b1',
+  '#840a9c',
+];
+
+const theme = createTheme({
+  colors: {
+    myColor,
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MantineProvider theme={theme}>
+      <ModalsProvider>
+        <Notifications position='bottom-right' />
+        <Component {...pageProps} />
+      </ModalsProvider>
+    </MantineProvider>
+  );
 }
