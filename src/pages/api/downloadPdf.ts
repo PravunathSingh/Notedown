a631ffd3dd6c3b77f11b016cbba1db65f119e481
pravunathsingh <pravunathsingh@gmail.com>
2023-12-10 19:32:48 +0530

    :bug: fix(editor): fixed api route bug

--- src/pages/api/downloadPdf.ts
+++ src/pages/api/downloadPdf.ts
@@ -3,7 +3,14 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { convertTailwindClassesToCss } from '@/apiUtils/convertTailwindClassesToCss';
 import puppeteer from 'puppeteer';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== 'POST') {
+    res.status(404).json({ message: 'Not found' });
+    return;
+  }
   const { fileName, doc } = req.body;
   if (!fileName || !doc) {
     res.status(400).json({ message: 'Missing fileName or doc' });
@@ -11,30 +18,50 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
 
   const htmlWithStyles = convertTailwindClassesToCss(doc);
 
-  (async () => {
-    const browser = await puppeteer.launch();
+  // (async () => {
+  //   const browser = await puppeteer.launch();
 
-    const page = await browser.newPage();
-    await page.setContent(htmlWithStyles);
-    await page.emulateMediaType('screen');
+  //   const page = await browser.newPage();
+  //   await page.setContent(htmlWithStyles);
+  //   await page.emulateMediaType('screen');
 
-    const pdf = await page.pdf({
-      path: `./public/${fileName}.pdf`,
-      format: 'A4',
-      printBackground: true,
-      margin: { top: '1cm', right: '1cm', bottom: '1cm', left: '1cm' },
-    });
+  //   const pdf = await page.pdf({
+  //     path: `./public/${fileName}.pdf`,
+  //     format: 'A4',
+  //     printBackground: true,
+  //     margin: { top: '1cm', right: '1cm', bottom: '1cm', left: '1cm' },
+  //   });
 
-    await browser.close();
+  //   await browser.close();
 
-    // send the pdf as a response
-    res.setHeader('Content-Type', 'application/pdf');
-    res.setHeader(
-      'Content-Disposition',
-      `attachment; filename=${fileName}.pdf`
-    );
-    res.send(pdf);
-  })();
+  //   // send the pdf as a response
+  //   res.setHeader('Content-Type', 'application/pdf');
+  //   res.setHeader(
+  //     'Content-Disposition',
+  //     `attachment; filename=${fileName}.pdf`
+  //   );
+  //   res.send(pdf);
+  // })();
 
-  res.status(200);
+  // res.status(201).json({ message: 'ok' });
+
+  const browser = await puppeteer.launch();
+
+  const page = await browser.newPage();
+  await page.setContent(htmlWithStyles);
+  await page.emulateMediaType('screen');
+
+  const pdf = await page.pdf({
+    path: `./public/${fileName}.pdf`,
+    format: 'A4',
+    printBackground: true,
+    margin: { top: '1cm', right: '1cm', bottom: '1cm', left: '1cm' },
+  });
+
+  await browser.close();
+
+  // send the pdf as a response
+  res.setHeader('Content-Type', 'application/pdf');
+  res.setHeader('Content-Disposition', `attachment; filename=${fileName}.pdf`);
+  res.status(201).send(pdf);
 }
