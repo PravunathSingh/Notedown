6697c9fca639ef9f43d3f2fca41187096add6616
pravunathsingh <pravunathsingh@gmail.com>
2023-12-12 11:19:42 +0530

    :zap: perf(editor): replaced puppeteer with puppeteer core

--- src/pages/api/downloadPdf.ts
+++ src/pages/api/downloadPdf.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
-import puppeteer from 'puppeteer';
+import puppeteer from 'puppeteer-core';
 
 export default async function handler(
   req: NextApiRequest,
